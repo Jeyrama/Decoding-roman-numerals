cad8ca0d19352aa4e93124786b7cebc913cd525a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-21 13:37:48 -0800

    Update Decoding-roman-numerals.js

--- Decoding-roman-numerals.js
+++ Decoding-roman-numerals.js
@@ -53,3 +53,22 @@ function solution(roman) {
 }
 
 // or
+
+const numeralMap = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function solution(roman){
+  const arrNums = roman.split('').map((el) => numeralMap[el]);
+  
+  return arrNums.reduce((sum, cur, i, a) => {
+    if (cur < a[i+1]) return sum - cur;
+    return sum + cur;
+  }, 0)
+}
